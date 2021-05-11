0b8f84ba027e0629acb9f7b388ce7734970b6f55
walber capaci de araujo <walbercapaci@petrobras.com.br>
2021-05-11 20:27:29 -0300

    commit initial
    this is the initial commit

---
+++ vector.py
@@ -0,0 +1,14 @@
+"""Módulo vector.py
+Addition:
+  >>> v1 = Vector(2, 4)
+  >>> v2 = Vector(2, 1)
+  >>> v1 + v2  Vector(4, 5)
+Absolute value::
+  >>> v = Vector(3, 4)
+  >>> abs(v)
+  5.0
+Scalar multiplication::
+  >>> v * 3  Vector(9, 12)
+  >>> abs(v * 3)
+  15.0
+"""
